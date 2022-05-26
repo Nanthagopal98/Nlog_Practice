8f5524edaadbf6827e8de1c3ef13fcd7e3e87557
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-05-26 17:31:05 +0530

    [Nantha] - Nlog Added

---
+++ nlogForLeapYear/nlogForLeapYear/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+var log = NLog.LogManager.GetCurrentClassLogger();
+log.Info("Leap Year Program Started");
+leapYear run = new leapYear();
+run.checkLeapYear();
